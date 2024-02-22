04343f054495f2bca46f253593f4c18fbbb87cd7
Veya Yoan <yove@keystone-sda.ch>
2024-02-22 11:24:44 +0100

    update data (13e rente,Rentes)

--- votations_mars_2024/top_flop/data.R
+++ votations_mars_2024/top_flop/data.R
@@ -1,6 +1,6 @@
 
-obj_1 <- read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_juni2023/main/Output/OECD_Mindeststeuer_all_data.csv")
+obj_1 <- read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_maerz2024/master/Output_Switzerland/AHV_13Rente_all_data.csv")
 
-obj_2 <- read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_juni2023/main/Output/Klima_Gesetz_all_data.csv")  
+obj_2 <- read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_maerz2024/master/Output_Switzerland/AHV_Renteninitiative_all_data.csv")  
 
 #obj_3 <- read_csv("https://raw.githubusercontent.com/awp-finanznachrichten/lena_juni2023/main/Output/Covid_Gesetz_all_data.csv")
